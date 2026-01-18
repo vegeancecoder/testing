e19605e4a6c2c13e05ba4f578315c39de757dbb2
nathan1el-t <nathanielteo1130@gmail.com>
2026-01-18 14:45:32 +0800

    testing merge

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-def test(num2):
-    if num2 ==1:
-        return num2
-    return num2+test(num2)
-
-def newfunc(num):
-    return num
-
-def thirdfunc(new):
-    return "hello world"
+def newfunc(test):
+    return test + 1
